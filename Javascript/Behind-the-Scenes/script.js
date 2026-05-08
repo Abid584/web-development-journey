b448a354b643f50c5013ffb1c58ca121bce4ee29
Abid584 <sheikhabidali391@gmail.com>
2026-05-08 13:46:24 +0500

    Update script.js

--- Javascript/Behind-the-Scenes/script.js
+++ Javascript/Behind-the-Scenes/script.js
@@ -9,7 +9,7 @@ function calcAge(birthYear) {
 
   function printAge() {
     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+    console.log(output); 
 
     if (birthYear >= 1981 && birthYear <= 1996) {
       var millenial = true;
